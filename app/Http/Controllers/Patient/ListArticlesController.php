aa824058dfc526795a7154c6d47536dd8c23726a
MrAisenodni <fikrialfayed@gmail.com>
2023-06-12 07:57:57 +0700

    fixing detail best practice without login

--- app/Http/Controllers/Patient/ListArticlesController.php
+++ app/Http/Controllers/Patient/ListArticlesController.php
@@ -36,11 +36,7 @@ class ListArticlesController extends Controller
             'reviews'   => $this->count_history->selectRaw('type, SUM(count) AS count')->where('disabled', 0)->orderBy('type')->groupBy('type')->get(),
             'review'    => $this->count_history->select('count')->where('foreign_id', $id)->where('disabled', 0)->where('type', 'article')->first(),
             'search'    => $search,
-        ];
-        $data['access'] = $this->menu_access->select('view', 'add', 'edit', 'delete', 'detail', 'approval')->where('disabled', 0)
-            ->where('role', session()->get('srole'))->where('menu_id', $data['c_menu']->id)->first();
-        if ($data['access']->view == 0) abort(403);
-        
+        ];        
         ($count) ? $this->count_history->where('id', $count->id)->update(['count' => $count->count + 1])
             : $this->count_history->insert(['type' => 'article', 'foreign_id' => $id, 'count' => 1]);
 
